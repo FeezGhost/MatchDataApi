89423dfe660e8702975973ebfbc25f729d46dc59
feezghost <feezoocrazy420@gmail.com>
2022-08-22 03:58:04 +0500

    allow cors

--- main.py
+++ main.py
@@ -4,8 +4,19 @@ from typing import Optional, Union
 from urllib import response
 from fastapi import FastAPI, status, Response
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Create Your Response Object
 class MatchDataObj(BaseModel):
